2d37fcd8f1c97a1806c135e78b9038047771ecc6
Jakub Pelák <jpelak@gmail.com>
2025-02-21 09:54:20 +0100

    fix: multilang support

--- index.php
+++ index.php
@@ -7,17 +7,17 @@ Kirby::plugin('lemmon/kirby-panel-buttons', [
     'fields' => [
         'buttons' => [
             'props' => [
-                'label'     => fn ( string $x = 'Button'     ) => I18n::translate($x, $x, kirby()->user()?->language()),
-                'text'      => fn ( string $x = 'Click here' ) => I18n::translate($x, $x, kirby()->user()?->language()),
-                'help'      => fn ( string $x = null         ) => I18n::translate($x, $x, kirby()->user()?->language()),
-                'url'       => fn ( string $x = '/'          ) => $x,
-                'theme'     => fn ( string $x = null         ) => $x,
-                'fullwidth' => fn ( bool   $x = false        ) => $x,
-                'size'      => fn ( string $x = 'lg'         ) => $x,
-                'icon'      => fn ( string $x = null         ) => $x,
-                'disabled'  => fn ( bool   $x = false        ) => $x,
-                'open'      => fn ( bool   $x = false        ) => $x,
-                'reload'    => fn ( bool   $x = false        ) => $x,
+                'label'     => fn ( string|array|null $x = 'Button'     ) => I18n::translate($x, $x, kirby()->user()?->language()),
+                'text'      => fn ( string|array|null $x = 'Click here' ) => I18n::translate($x, $x, kirby()->user()?->language()),
+                'help'      => fn ( string|array|null $x = null         ) => I18n::translate($x, $x, kirby()->user()?->language()),
+                'url'       => fn ( string            $x = '/'          ) => $x,
+                'theme'     => fn ( string|null       $x = null         ) => $x,
+                'fullwidth' => fn ( bool              $x = false        ) => $x,
+                'size'      => fn ( string            $x = 'lg'         ) => $x,
+                'icon'      => fn ( string|null       $x = null         ) => $x,
+                'disabled'  => fn ( bool              $x = false        ) => $x,
+                'open'      => fn ( bool              $x = false        ) => $x,
+                'reload'    => fn ( bool              $x = false        ) => $x,
             ],
             'computed' => [
                 'text' => fn () => $this->model()->toSafeString($this->text),
